1ef150b63cd0271dfd2b7b82f646be2a6d586c9f
Rogerio_Margoni <margoni@gmail.com>
2019-02-18 22:06:27 -0300

    Exercicio de conexão com PreparedStatement

--- projeto-banco/src/main/java/com/target/treinamento/projeto_banco/App.java
+++ projeto-banco/src/main/java/com/target/treinamento/projeto_banco/App.java
@@ -2,6 +2,7 @@ package com.target.treinamento.projeto_banco;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -42,43 +43,46 @@ public class App
 
 			
 			//passa o comando SQL
-			boolean deuCerto = statement.execute("update pessoas set primeiro_nome = 'Rogerio fazendo Update' where id = 2");
-
-			System.out.println(" ");
-
-			if (deuCerto) {
-				System.out.println("Funcionou");
-			} else {
-				System.out.println("Não foi");
-			}
+			boolean deuCerto = statement.execute("update pessoas set primeiro_nome = 'Rogerio Update' where id = 2");
 
 			//passa o comando SQL
-			boolean fezInsert = statement.execute("insert into pessoas (primeiro_nome, segundo_nome, endereco, cidade) values ('Oiregor', 'Oiregor', 'Protasio Alves', 'Porto Alegre')");
+			//boolean fezInsert = statement.execute("insert into pessoas (primeiro_nome, segundo_nome, endereco, cidade) values ('Oiregor', 'Oiregor', 'Protasio Alves', 'Porto Alegre')");
 
-			System.out.println(" ");
 
-			if (fezInsert) {
-				System.out.println("Funcionou o Insert");
+			// PreparedStatement
+			String sql = "insert into pessoas (primeiro_nome, segundo_nome, endereco, cidade) values (? , ? , ?, ?)";
+			PreparedStatement statement1 = connection.prepareStatement(sql);
+			statement1.setString(1, "Pedrinho");
+			statement1.setString(2, "Alfredo");
+			statement1.setString(3, "Rua Bendo Rosa");
+			statement1.setString(4, "Lajeado");
+			
+			int retorno = statement1.executeUpdate();
+
+			System.out.println(" ");			
+			
+			if (retorno == 1 ) {
+				System.out.println("Sucesso!");
 			} else {
-				System.out.println("Não foi o Insert");
-			}
-			
-
+				System.out.println("Erro ao gravar dados!");
+			}			
 			
 			System.out.println(" ");
-
 			
-			while (resultSet.next()) {
-				String primeiro_nome = resultSet.getString("primeiro_nome");
-				String segundo_nome = resultSet.getString("segundo_nome");
-				String endereco = resultSet.getString("endereco");
-				String cidade = resultSet.getString("cidade");
-				Long id = resultSet.getLong("id");
+			ResultSet resultSet2 = statement.executeQuery("select id, primeiro_nome, segundo_nome, endereco, cidade from pessoas order by id");
+			
+			while (resultSet2.next()) {
+				String primeiro_nome = resultSet2.getString("primeiro_nome");
+				String segundo_nome = resultSet2.getString("segundo_nome");
+				String endereco = resultSet2.getString("endereco");
+				String cidade = resultSet2.getString("cidade");
+				Long id = resultSet2.getLong("id");
 
 				System.out.println(id + ", "+primeiro_nome+" , "+ segundo_nome+" , "+ endereco+" , "+ cidade);
 			}
 
 			statement.close();
+			statement1.close();
 			connection.close();
 			
 			
